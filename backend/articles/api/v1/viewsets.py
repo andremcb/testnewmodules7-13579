ff6d71ba72d5d3cb06404cf791fa1a61a52e775c
André Machado <csantos.machado@gmail.com>
2020-12-17 19:57:15 +0000

    Update models

---
+++ backend/articles/api/v1/viewsets.py
@@ -0,0 +1,13 @@
+from rest_framework import authentication
+from articles.models import Article
+from .serializers import ArticleSerializer
+from rest_framework import viewsets
+
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Article.objects.all()
